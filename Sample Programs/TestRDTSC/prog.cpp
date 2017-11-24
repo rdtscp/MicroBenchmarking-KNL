9f11ffd8db6b1b3603bb802b9ab37bd456c8b10f
Alexander Wilson <acwilson96@gmail.com>
2017-11-24 18:26:48 +0000

    Removed comments for reading

--- Sample Programs/TestRDTSC/prog.cpp
+++ Sample Programs/TestRDTSC/prog.cpp
@@ -6,17 +6,6 @@
 
 cpu_set_t mask;
 
-// int loneSize = 32768;
-// int numElems = 4096;
-
-// int data[4096];
-
-// static inline uint64_t rdtsc() {
-//     uint64_t out;
-//     asm("rdtsc" : "=A"(out));
-//     return out;
-// }
-
 static inline uint64_t test_rdtsc() {    
     uint64_t a;
     uint64_t b;
@@ -33,30 +22,9 @@ inline void assignToCore(int core_id) {
     sched_setaffinity(0, sizeof(mask), &mask);
 }
 
-// void init() {
-//     for (int i=0; i < numElems; i++) {
-//         data[i] = 1;
-//     }
-// }
-
-// void run() {
-//    uint64_t before;
-//    uint64_t after;
-//    int temp = 0;
-//    for (int i=0; i < numElems; i++) {
-//        before = rdtsc();
-//        temp = data[i];
-//        after = rdtsc();
-//     //    printf("%" PRIu64 "\n", after - before);
-//    }
-// }
-
 int main() {
     assignToCore(0);
     for (int i = 0; i < 100; i++) {
         printf("%" PRIu64 "\n", test_rdtsc());
     }
-    // printf("test_rdtsc(): %" PRIu64 "\n", test_rdtsc());
-    // init();
-    // run();
 }
