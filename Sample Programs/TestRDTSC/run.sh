6b4887e343b69e2d001e31c9452b8a68912b6539
s1421803 <s1421803@Alexanders-MacBook-Pro.local>
2018-01-19 02:34:21 +0000

    Update test format

--- Sample Programs/TestRDTSC/run.sh
+++ Sample Programs/TestRDTSC/run.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 g++ prog10.cpp
 ./a.out
-g++ prog50.cpp
-./a.out
-g++ prog100.cpp
-./a.out
 g++ prog10_repeat.cpp
 ./a.out
+
+g++ prog50.cpp
+./a.out
 g++ prog50_repeat.cpp
+./a.out
+
+g++ prog100.cpp
 ./a.out
 g++ prog100_repeat.cpp
 ./a.out
