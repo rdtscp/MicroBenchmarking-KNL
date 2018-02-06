0281b5878ec1df48038a389615562c944bd4055a
Alexander Wilson <acwilson96@gmail.com>
2018-02-06 09:55:32 +0000

    lat-ns converter

---
+++ Sample Programs/TestRDTSC/lat_conv.js
@@ -0,0 +1,10 @@
+let freq = 2400000000;
+
+let arg3 = process.argv[2];
+
+let cycles = arg3;
+let secs = cycles/freq;
+console.log(cycles + " Cycles\t@ 2.4GHz\t= " + (secs*1000000000).toFixed(2) + "ns");
+let ns = arg3;
+secs = ns / 1000000000;
+console.log(ns + " Nano(s)\t@ 2.4GHz\t= " + (secs * freq).toFixed(2) + " Cycles");
