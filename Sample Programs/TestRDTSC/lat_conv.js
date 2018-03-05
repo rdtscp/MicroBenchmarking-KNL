4ef9dd2a35a93a431cc13aaf70465863c007d326
Alexander Wilson <acwilson96@gmail.com>
2018-03-05 08:44:38 +0000

    updated output

--- Sample Programs/TestRDTSC/lat_conv.js
+++ Sample Programs/TestRDTSC/lat_conv.js
@@ -4,7 +4,7 @@ let arg3 = process.argv[2];
 
 let cycles = arg3;
 let secs = cycles/freq;
-console.log(cycles + " Cycles\t@ 2.4GHz\t= " + (secs*1000000000).toFixed(2) + "ns");
+console.log(cycles + " Cycles\t@ " + freq + "Hz\t= " + (secs*1000000000).toFixed(2) + "ns");
 let ns = arg3;
 secs = ns / 1000000000;
-console.log(ns + " Nano(s)\t@ 2.4GHz\t= " + (secs * freq).toFixed(2) + " Cycles");
+console.log(ns + " Nano(s)\t@ " + freq + "Hz\t= " + (secs * freq).toFixed(2) + " Cycles");
